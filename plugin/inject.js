9a14e5a44247edb1217cf0102b9680cff0070d60
JaredMathis <jaredmathis@protonmail.com>
2020-05-21 09:07:29 -0400

    update

--- plugin/inject.js
+++ plugin/inject.js
@@ -1,14 +1,33 @@
 // this is the code which will be injected into a given page...
 
 (function() {
+    function log(message) {
+        console.log('injected: ' + message);
+    }
+    
+    if (isIndex()) {
+        indexClickRow(state);
+    } else {
+        log('not index');
+    }
 
-    console.log('injected: ', {state});
-    
-    
+    if (isPortfolio()) {
+        log('portfolio')
+    } else {
+        log('not portfolio');
+    }
+
+    function indexClickRow(state) {
+        log('indexClickRow: entered');
+
+        let submit = document.getElementById("submit");
+
+        submit.click();
+    }
 
     function isIndex() {
         let submit = document.getElementById("submit");
-        console.log({submit});
+        return !!submit;
     }
 
 })();
