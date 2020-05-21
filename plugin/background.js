c4c8ca26f5b00fdd17d768187e6681820e814ca7
JaredMathis <jaredmathis@protonmail.com>
2020-05-21 09:36:40 -0400

    update

--- plugin/background.js
+++ plugin/background.js
@@ -1,17 +1,40 @@
 // this is the background code...
 
+let active = false;
+
 // listen for our browerAction to be clicked
 chrome.browserAction.onClicked.addListener(function (tab) {
+    active = !active;
+    console.log('clicked', {active})
+
+    run();
+});
+
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+    if (changeInfo.status !== 'complete') {
+        return;
+    }
+    console.log({active})
+    if (!active) {
+        return;
+    }
+
+    run();
+  })
+
+  function run() {
+
+
     let state = {
         entityRow: 1,
     };
 
-    chrome.tabs.executeScript(tab.id, {
+    chrome.tabs.executeScript(null, {
         code: 'var state = ' + JSON.stringify(state)
     }, function() {
         // for the current tab, inject the "inject.js" file & execute it
-        chrome.tabs.executeScript(tab.ib, {
+        chrome.tabs.executeScript(null, {
             file: 'inject.js'
         });
     });
-});
+  }
--- plugin/inject.js
+++ plugin/inject.js
@@ -12,9 +12,59 @@
     }
 
     if (isPortfolio()) {
-        log('portfolio')
+        portfolioClickPrivateCRR(state);
+        portfolioClickDownloads(state);
     } else {
         log('not portfolio');
+    }
+
+    if (isDownloads()) {
+        downloadsDownloadCSV(state);
+        downloadsDownloadXML(state);
+        ReturnToEntityAndRoleSelection(state);
+    } else {
+        log('not downloads');
+    }
+
+    function ReturnToEntityAndRoleSelection(state) {
+        let r = document.getElementById("return");
+        r.click();
+    }
+
+    function downloadsDownloadXML(state) {
+        let downloadXML = document.getElementById("downloadXML");
+        downloadXML.setAttribute('download', 'XML-renamed.csv');
+        downloadXML.click();
+    }
+
+    function downloadsDownloadCSV(state) {
+        let downloadCSV = document.getElementById("downloadCSV");
+        downloadCSV.setAttribute('download', 'CSV-renamed.csv');
+        downloadCSV.click();
+    }
+
+    function isDownloads() {
+        let downloadCSV = document.getElementById("downloadCSV");
+        return !!downloadCSV;
+    }
+
+    function portfolioClickDownloads() {
+        log('portfolioClickDownloads: entered');
+
+        let downloads = document.getElementById("downloads");
+        downloads.click();
+    }
+
+    function portfolioClickPrivateCRR() {
+        log('portfolioClickPrivateCRR: entered');
+
+        let privateCRR = document.getElementById("privateCRR");
+        privateCRR.click();
+    }
+
+    function isPortfolio() {
+        let privateCRR = document.getElementById("privateCRR");
+        return !!privateCRR;
     }
 
     function indexClickRow(state) {
